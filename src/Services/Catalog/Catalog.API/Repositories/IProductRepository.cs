4eea28bcb149e222b28b040b0105c0ec511892b6
vdimov <vdimov@objectsystems.com>
2021-04-08 23:46:29 +0300

    catalog service implemented

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Repositories
+{
+    using Catalog.API.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IProductRepository
+    {
+        Task CreateProduct(Product product);
+        Task<Product> GetProduct(string id);
+        Task<bool> DeleteProduct(string id);
+        Task<bool> UpdateProduct(Product product);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
