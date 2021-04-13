83664c5bd280432d4a370edb504a31f963a2a490
vdimov <vdimov@objectsystems.com>
2021-04-13 08:57:25 +0300

    basket api added

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Entities
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+    }
+}
